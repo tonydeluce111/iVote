ec21caa878c22e5b739647b846f2d5831b012d8d
tonydeluce111 <tondeluce111@gmail.com>
2015-08-10 20:58:41 -0700

    implemented IVoteService with functions for checking answers and tallying number of correct answers.

--- IVoteService.java
+++ IVoteService.java
@@ -1,5 +1,27 @@
 package us.deluce.ivote;
 
-public class IVoteService {
+import java.util.HashMap;
+import java.util.Map;
 
+public class IVoteService {
+	
+	private Question question;
+	private Map<String, String> studentSubmissions;
+	private int numberCorrect;
+	
+	public IVoteService(Question question) {
+		this.question = question;
+		this.studentSubmissions = new HashMap<String, String>();
+		this.numberCorrect = 0;
+	}
+	
+	public void studentSubmission(String studentId, String submission) {	
+		studentSubmissions.put(studentId, submission);
+		if (question.correctAnswer.equals(submission)) numberCorrect++;
+	}
+	
+	public int getNumberCorrect() {
+		return numberCorrect;
+	}
+	
 }
